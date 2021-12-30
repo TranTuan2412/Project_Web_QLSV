ec9cabe1d3adb7201f981dc4815ec85c2943bed3
Your Name <you@example.com>
2021-12-30 22:58:39 +0700

    commit 30/12

--- app/model/teacher.php
+++ app/model/teacher.php
@@ -2,4 +2,22 @@
     require '../common/db.php';
     $teachers = $conn->query("SELECT * FROM `teachers` order by id");
 	$teachers->execute();
+
+    function deleteData($id) {
+        global $conn;
+        $query = $conn->prepare ("DELETE FROM teachers WHERE id=" . $id);
+        $query->execute();
+        return 1;
+    }
+    function searchData($keyword, $specialized) {
+        global $conn;
+        if( $specialized != "") {
+            $query = $conn->prepare("SELECT * FROM `teachers` WHERE (`name` LIKE '%$keyword%' or `description` LIKE '%$keyword%' or `degree` LIKE '%$keyword%') and `specialized` = '$specialized' order by id");
+        }else {
+            $query = $conn->prepare("SELECT * FROM `teachers` WHERE `name` LIKE '%$keyword%' or `description` LIKE '%$keyword%' or `degree` LIKE '%$keyword%' order by id");
+        }
+        $query->execute();
+        $rowAll = $query->fetchAll();
+        return $rowAll;
+    }
 ?>
--- app/view/teacher_search_input.php
+++ app/view/teacher_search_input.php
@@ -1,112 +1,44 @@
-<!DOCTYPE html>
+<?php
+require '../common/define.php';
+?>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tìm kiếm giáo viên</title>
     <link rel="stylesheet" href="../../web/css/teacher/teacher_search.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <title>Tìm kiếm phòng học</title>
 </head>
+
 <body>
-    <?php
-        require '../controller/teacher_search.php';
-        require '../common/define.php';
-       
-    ?>
-    <div class="content container">
-        <div class='col-md-12'>
-            <form name='formsearch' action='../controller/classroom_search.php' method='GETs'>
-                <div class="col-md-12 filterbuilding">
-                    <div class="col-md-2"></div>
-                    <div class="col-sm-2">
-                        <div class="word">Bộ Môn</div>
-                    </div>
-                    <div class="col-sm-6">
-					<select name='teacherSpecialized' id='idSpecialized'>
-						<option></option>
-						<?php foreach ($listSpecialized as $key => $specialized) : ?>
-							<option>
-								<?php echo $specialized; ?>
-							</option>
-						<?php endforeach; ?>
-					</select>
-                    </div>
+
+    <div class="container">
+        <form action="" method="GET">
+            <div class="search">
+                <div class="search__specialized">
+                    <label for="">Chuyên ngành</label>
+                    <select id="specialized" class="specialized__select" name="specialized">
+                        <option value=''></option>
+                        <?php
+                        foreach ($listSpecialized as $key => $value) {
+                            echo "<option value='$key'>$value</option>";
+                        }
+                        ?>
+                    </select>
                 </div>
-                <div class="col-md-12 seacrch">
-                    <div class="col-md-2"></div>
-                        <div class="col-sm-2">
-                            <div class="word">Từ Khóa</div>
-                        </div>
-                        <div class="col-sm-6">
-                            <label for="" style='width:100%'>
-                                <input type="text" class="filter" id="filter" name="filter">
-                            </label>
-                        </div>
+                <div class="search__keyword">
+                    <label for="">Từ khóa</label>
+                    <input type="search" name="keyword" class="keyword__input" value="<?php echo isset($_GET['keyword']) ? $_GET['keyword'] : '' ?>">
                 </div>
-                <div class="col-md-12" style='text-align: center;margin-top:20px'>
-                        <button  type="button" class="filtervalue"  id="btn-search" name="search" >Tìm Kiếm</button>
+                <div class="search__btn">
+                    <input type="submit" name="search" value="Tìm kiếm" class="btn__submit">
                 </div>
-            </form>
-			<?php count=0; ?>
-			<?php foreach ($teachers as $teacher) : ?>
-				<?php count++; ?>
-			<?php endforeach; ?>
-			<div class="count_teacher">
-            	<p>Số giáo viên tìm thấy: <?php echo $count ?></p>
-        	</div>				
-            <div class="col-md-12">
-                <table id="admin">
-                    <tr class="header">
-                        <th style="width:5%;">No
-                        </th>
-                        <th style="width:20%;">Tên giáo viên
-                        </th>
-                        <th style="width:10%;">Khoa
-                        </th>
-                        <th style="width:40%;">Mô tả chi tiết
-                        </th>
-                        <th style="width:20%;"> Action
-                        </th>
-                    </tr>
-                    <?php foreach ($teachers as $teacher) : ?>
-                        <tr>
-                            <form action="../controller/teacher_search.php" 
-                                  method='POST'>
-                                <td>
-                                    <?php echo $teacher['id']?>
-                                    <input type="hidden" 
-                                           name='id' 
-                                           value='<?php echo $teacher['id']?>' >
-                                </td>
-                                <td>
-                                    <?php echo $teacher['name']?>
-                                </td>
-                                <td>
-                                    <?php foreach ($listDegree as $key=>$degree) : ?>
-                                        <?php 
-                                            if($teacher['degree'] == $key){
-                                                echo $degree;
-                                            }
-                                        ?>
-                                    <?php endforeach; ?>
-                                </td>
-                                <td><?php echo $teacher['description']?></td>
-                                <td>
-                                    <button type='submit' 
-                                            name='btnDelete'>Xóa</button>
-                                    <button type='button' 
-                                            name='btnEdit' 
-                                            onclick='window.location.href="?id="+"<?php echo $teacher['id']?>"'
-                                             >Sửa</button>
-                                </td>
-                            </form>
-                        </tr>
-                    <?php endforeach; ?>
-                </table>
             </div>
-        </div>
+        </form>
+        <?php require '../controller/teacher_search.php'; ?>
     </div>
-    <script type="text/javascript" src=""></script>
+    <script src=""></script>
 </body>
+
 </html>

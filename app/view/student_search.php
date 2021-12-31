821305fe33367fa871371d5bdd772cd7ad6ffd7c
tungns <ngstug@gmail.com>
2021-12-31 17:14:46 +0700

    update UI search student

--- app/view/student_search.php
+++ app/view/student_search.php
@@ -5,36 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="css" href="/web/student/student.css">
-    <title>Tìm kiếm phòng học</title>
+    <link rel="stylesheet" href="../../web/student/student.css">
+    <title>Tìm kiếm sinh viên</title>
 </head>
 <body>
     <?php
         require '../controller/student_search.php';
         require '../common/define.php';
     ?>
-    <div class="content container">
+    <div class="search-student container">
         <div class="">
-            <form name='formsearch' action='../controller/student_search.php' method='GETs'>
-                <div class="seacrch">
-                    <div class="col-md-2"></div>
-                        <div class="col-sm-2">
-                            <div class="word">Từ Khóa</div>
-                        </div>
-                        <div class="col-sm-6">
-                            <label for="" style='width:100%'>
-                                <input type="text" class="filter" id="filter" name="filter">
-                            </label>
-                        </div>
+            <form name='search' action='../controller/student_search.php' method='GETs'>
+                <div class="key-input">
+                    <div class="col-sm-2">
+                        <div class="key-word">Từ Khóa</div>
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" class="filter" id="filter" name="filter">
+                    </div>
                 </div>
-                <div class="" style='text-align: center;margin-top:20px'>
-                        <button  type="button" class="filtervalue"  id="btn-search" name="search" >Tìm Kiếm</button>
+                
+                <div class="col-md-12 button-css ">
+                        <button  type="button" class="button-search"  id="btn-search" name="search" >Tìm Kiếm</button>
                 </div>
             </form>
         
             <div class="">
-                <table id="admin">
-                    <tr class="header">
+                <table class="">
+                    <tr class="">
                         <th style="width:5%;">No
                         </th>
                         <th style="width:35%;">Tên sinh viên+
@@ -46,23 +44,18 @@
                     </tr>
                     <?php foreach ($listStudents as $student) : ?>
                         <tr>
-                            <form action="../controller/student_search.php" 
-                                  method='POST'>
+                            <form action="../controller/student_search.php" method='POST'>
                                 <td>
                                     <?php echo $student['id']?>
-                                    <input type="hidden" 
-                                           name='id' 
-                                           value='<?php echo $student['id']?>' >
+                                    <input type="hidden" name='id' value='<?php echo $student['id']?>' >
                                 </td>
                                 <td>
                                     <?php echo $student['name']?>
                                 </td>
                                 <td><?php echo $student['description']?></td>
-                                <td>
-                                    <button type='submit' 
-                                            name=''>Xóa</button>
-                                    <button type='button' 
-                                            name='btnEdit' 
+                                <td class="edit-delete">
+                                    <button type='submit' style="margin-right: 2rem;" name=''>Xóa</button>
+                                    <button type='button' name='btnEdit' 
                                             onclick='window.location.href="student_edit.php?id="+"<?php echo $student['id'] ?>"'
                                              >Sửa</button>
                                 </td>

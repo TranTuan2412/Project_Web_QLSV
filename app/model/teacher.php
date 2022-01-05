6e39eac1f4654cbed366a6237353c6780911cc1a
Your Name <you@example.com>
2022-01-05 22:18:55 +0700

    commit 5/1
    update delete teacher

--- app/model/teacher.php
+++ app/model/teacher.php
@@ -1,8 +1,12 @@
 <?php
     require '../common/db.php';
-    $teachers = $conn->query("SELECT * FROM `teachers` order by id");
-	$teachers->execute();
-
+    function showTable(){
+        global $conn;
+        $query = $conn->query("SELECT * FROM `teachers` order by id");
+	    $query->execute();
+        $teachers = $query->fetchAll();
+        return $teachers;
+    }
     function deleteData($id) {
         global $conn;
         $query = $conn->prepare ("DELETE FROM teachers WHERE id=" . $id);
@@ -13,11 +17,17 @@
         global $conn;
         if( $specialized != "") {
             $query = $conn->prepare("SELECT * FROM `teachers` WHERE (`name` LIKE '%$keyword%' or `description` LIKE '%$keyword%' or `degree` LIKE '%$keyword%') and `specialized` = '$specialized' order by id");
-        }else {
+            $query->execute();
+        }else if ($keyword != ""){
             $query = $conn->prepare("SELECT * FROM `teachers` WHERE `name` LIKE '%$keyword%' or `description` LIKE '%$keyword%' or `degree` LIKE '%$keyword%' order by id");
+            $query->execute();
         }
-        $query->execute();
-        $rowAll = $query->fetchAll();
-        return $rowAll;
+        if ($keyword == "" && $specialized == "none" ){
+            $sqlTeacher = 'SELECT * FROM `teachers`';
+            $query = $conn ->prepare($sqlTeacher);
+            $query->execute();
+        }
+        $rowTeachers = $query->fetchAll();
+        return $rowTeachers;
     }
 ?>

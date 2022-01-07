f845456c1d98e5157c967b53d974920d13eda0a9
VinhNgo <49408017+Klyvngo@users.noreply.github.com>
2022-01-07 13:51:41 +0700

    Add files via upload

--- app/model/student.php
+++ app/model/student.php
@@ -1,20 +1,12 @@
 <?php
     require "../common/db.php";
-
-
-
-
-
-
-
-
-
-
     function getData($id){
         global $conn;
         $qr = "SELECT * FROM students where id=$id";
         $result = $conn->prepare($qr);
         $result->execute();
+        // $reponse = $conn->query($qr);
+        // $result = $reponse->fetch_assoc();
         return $result;
     }
     function updateData($id,$name,$description,$avatar){
@@ -22,5 +14,6 @@
         $qr = "UPDATE students SET name='$name', avatar='$avatar', description='$description', updated= now() WHERE id=$id";
         $result = $conn->query($qr);
         $result->execute();
+        // $conn->query($qr);
     }
 ?>

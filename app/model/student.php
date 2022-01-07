daacce3e08ae3f598e366958517abac49f42a9f3
TungTT <truongthanhtung2701.dev@gmail.com>
2022-01-07 13:37:43 +0700

    commit

--- app/model/student.php
+++ app/model/student.php
@@ -1,7 +1,5 @@
 <?php
     require '../common/db.php';
-    $result = $conn->query("SELECT * FROM students WHERE id='2'");
-    $result->execute();
     function add_student($name,$avatar,$des) {
         global $conn;
         $created = date("Y-m-d h:i:s");

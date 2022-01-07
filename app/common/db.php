e6b25f57101b6671937047eccf8a0795e8cd6003
Klyvngo <ngoquangvinh_t63@hus.edu.vn>
2022-01-07 14:38:24 +0700

    new branch

--- app/common/db.php
+++ app/common/db.php
@@ -9,9 +9,4 @@ try{
 } catch(PDOException $e){
     echo "Connection failed". $e->getMessage();
 }
-// $servername = '127.0.0.1:3306';
-// $username = 'root';
-// $password = 'Vinhanh-11';
-// $dbname = 'my_database';
-// $conn = new mysqli($servername,$username,$password,$dbname);
 ?>
--- app/controller/student_edit_input_controller.php
+++ app/controller/student_edit_input_controller.php
@@ -4,7 +4,7 @@
     $error = array('errorName'=> '','errorAvatar'=>'','errorDescription'=>'');
     require "../common/db.php";
     require "../model/student.php";
-    $_GET['idStudent']= 2;
+    // $_GET['idStudent']= 2;
     if(isset($_GET['idStudent'])){
         $idStudent = $_GET['idStudent'];
         $_SESSION['idStudent'] = $idStudent;

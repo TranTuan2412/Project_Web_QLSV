e56e63fed2f0f2edb3a6a589b35fd7a8aa5b0915
Your Name <you@example.com>
2022-01-09 00:58:24 +0700

    Update define.php
    fix define.php

--- app/common/define.php
+++ app/common/define.php
@@ -4,12 +4,12 @@
 		'02' => 'Năm 2',
 		'03' => 'Năm 3',
 		'04' => 'Năm 4',
+	];
     $listSpecialized = [
 		'001' => 'Khoa Học Máy Tính',
 		'002' => 'Khoa Học Dữ Liệu',
 		'003' => 'Hải Dương Học',
 	];
-
 	$listDegree = [
 		'001' => 'Cử nhân',
 		'002' => 'Thạc sĩ',

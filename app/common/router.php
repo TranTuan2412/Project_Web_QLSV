63651909b9fef82d2bedb0ea90ffc4f9590acbe6
tuannb19 <75989581+tuannb19@users.noreply.github.com>
2022-01-08 20:59:56 +0700

    Add files via upload

---
+++ app/common/router.php
@@ -0,0 +1,23 @@
+<?php 
+class Router{
+    public function setRouter($path){
+        switch ($path) {
+            case 'login':
+                require_once './app/controller/login.php';
+                break;
+        
+            case 'home':
+                require_once './app/controller/home.php';
+                break;
+            default:
+                // require_once './app/view/home.php';
+                break;
+        }
+    }
+}
+
+$path = isset($_REQUEST['router']) ? $_REQUEST['router'] : 'login';
+
+$router = new Router();
+$router-> setRouter($path);
+?>

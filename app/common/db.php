79680dd559ca4048f66fe44bfc391cb8f9064250
tuannb19 <75989581+tuannb19@users.noreply.github.com>
2022-01-08 21:29:51 +0700

    Add files via upload

--- app/common/db.php
+++ app/common/db.php
@@ -9,8 +9,8 @@
 
         public function __construct(){
             try {
-                $this->__conn = new PDO("mysql:host=$this->host;dbname=$this->DB", $this->user, $this->password);
-                $this->__conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->conn = new PDO("mysql:host=$this->host;dbname=$this->DB", $this->user, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 // echo "Connected successfully";
               } catch(PDOException $e) {
                 echo "Connection failed: " . $e->getMessage();

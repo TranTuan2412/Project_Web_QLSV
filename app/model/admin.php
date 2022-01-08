b3f922e7fa6385982c1e89043e6a99f4ad00b16a
tuannb19 <75989581+tuannb19@users.noreply.github.com>
2022-01-08 21:31:54 +0700

    Add files via upload

--- app/model/admin.php
+++ app/model/admin.php
@@ -4,13 +4,13 @@ require_once './app/common/db.php';
 class Admin extends DB{
     public function select_All_Admin(){
         $sql = "SELECT * FROM admins";
-        return $this->__conn->query($sql);        
+        return $this->conn->query($sql);        
     }
 
     
     public function select_All_LoginId($login_id){
         $sql = "SELECT * FROM admins where login_id='$login_id'";
-        return $this->__conn->query($sql);   
+        return $this->conn->query($sql);   
     }
     
 }

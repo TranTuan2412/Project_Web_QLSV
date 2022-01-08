240245a6a286817e6d92f753833dd76401e7e338
tuannb19 <75989581+tuannb19@users.noreply.github.com>
2022-01-08 22:01:58 +0700

    Add files via upload

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,6 @@
 <?php 
 define('APPROOT',dirname(dirname(__FILE__)));
-define('URLROOT','http://' . $_SERVER['HTTP_HOST'].'/quanlisinhvien');
+define('URLROOT','http://' . $_SERVER['HTTP_HOST'].'/Project_Web_QLSV');
 
 
 ?>
